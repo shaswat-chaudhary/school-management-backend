b576567beff112162a1957b39ebebb1db8cacb0b
iamshaswat <shaswatkumar281@bbdu.ac.in>
2025-12-26 14:14:11 +0530

    update db

--- database/db.js
+++ database/db.js
@@ -1,19 +1,23 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
   if (err) {
     console.error("Database connection failed:", err);
     return;
   }
   console.log("Database connected successfully");
+  connection.release();
 });
 
 
